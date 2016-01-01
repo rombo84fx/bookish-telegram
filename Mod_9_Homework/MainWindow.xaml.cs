2803fb18496e551a2f760fd335caa444f88e50a4
rombo84fx <gmart_nez@yahoo.com.mx>
2016-01-01 14:59:52 -0600

    Version submitted to edX

--- Mod_9_Homework/MainWindow.xaml.cs
+++ Mod_9_Homework/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace Mod_9_Homework
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students = new List<Student>();
+        int index = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            Student student1 = new Student(txtFirstName.Text, txtLastName.Text, txtCity.Text);
+            students.Add(student1);
+            index = students.Count - 1;
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+            MessageBox.Show("Student created");
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (students.Count != 0)
+            {
+                if (index == 0)
+                {
+                    index = students.Count - 1;
+                }
+                else
+                {
+                    index--;
+                }
+                populateTextBoxes(index);
+            }
+            else
+            {
+                MessageBox.Show("There are no students in the collection.");
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (students.Count != 0)
+            {
+                if (index == (students.Count - 1))
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+                populateTextBoxes(index);
+            }
+            else
+            {
+                MessageBox.Show("There are no students in the collection.");
+            }
+        }
+
+        private void populateTextBoxes(int index)
+        {
+            txtFirstName.Text = students[index].FirstName;
+            txtLastName.Text = students[index].LastName;
+            txtCity.Text = students[index].City;
         }
     }
 }
---
+++ Mod_9_Homework/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string city;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+
+            set
+            {
+                city = value;
+            }
+        }
+
+        public Student(string FirstName, string LastName, string City)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.City = City;
+        }
+    }
+}
